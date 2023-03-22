2389e82808278c1bc9610ed8baffa76e05a7b8ac
0xnexusflip <0xnexusflip@gmail.com>
2023-03-23 06:05:01 +0700

    add fetcher templates

--- src/apps/y-2-k-finance/y-2-k-finance.module.ts
+++ src/apps/y-2-k-finance/y-2-k-finance.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 
 import { AbstractApp } from '~app/app.dynamic-module';
 
+import { ArbitrumY2KFinanceFarmContractPositionFetcher } from './arbitrum/y-2-k-finance.farm.contract-position-fetcher';
+import { ArbitrumY2KFinanceLockContractPositionFetcher } from './arbitrum/y-2-k-finance.lock.contract-position-fetcher';
+import { ArbitrumY2KFinanceTokenTokenFetcher } from './arbitrum/y-2-k-finance.token.token-fetcher';
 import { Y2KFinanceContractFactory } from './contracts';
 
 @Module({
-  providers: [Y2KFinanceContractFactory],
+  providers: [
+    ArbitrumY2KFinanceFarmContractPositionFetcher,
+    ArbitrumY2KFinanceLockContractPositionFetcher,
+    ArbitrumY2KFinanceTokenTokenFetcher,
+    Y2KFinanceContractFactory,
+  ],
 })
 export class Y2KFinanceAppModule extends AbstractApp() {}
